005d7e9d3cb56e23ab3e5c104ee180471beb128d
deividarauj0 <deivid-ns@hotmail.com>
2023-06-15 11:32:09 -0300

    Aula_05

---
+++ app/javascript/controllers/clientes_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus";
+import renderClientesTable from "../templates/clientes/index";
+
+export default class extends Controller {
+  connect() {
+    this.loadClientes();
+  }
+
+  async loadClientes() {
+    const clientes = await (await fetch("http://[::1]:3000/clientes.json")).json();
+    const html = renderClientesTable(clientes);
+    this.element.innerHTML = html;
+  }
+}
+
---
+++ app/javascript/templates/clientes/index.js
@@ -0,0 +1,27 @@
+export default function renderClientesTable(clientes) {
+  return `
+    <h1 class="major">Clientes</h1>
+    <a href="#">Novo Cliente</a>
+    <br><br>
+    <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Telefone</th>
+            <th>Endereço</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${clientes.map(cliente => `
+            <tr>
+              <td>${cliente.id}</td>
+              <td>${cliente.nome}</td>
+              <td>${cliente.telefone}</td>
+              <td>${cliente.endereco}</td>
+            </tr>
+          `).join("")}
+        </tbody>
+    </table>
+  `;
+}
